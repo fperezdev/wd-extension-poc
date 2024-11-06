0f95ce8eb97869b625f57cd34d0a7953bee3d091
Francisco Perez Lefiman <fperez@wird.ai>
2024-11-06 19:39:19 -0300

    Change to outlook

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -8,7 +8,7 @@ const MainButton = () => {
   };
   return (
     <Button
-      className="w-[50px] h-[50px] absolute top-[calc(50%-25px)] right-10 pointer-events-auto text-3xl bg-pink-600 text-white rounded-full"
+      className="w-[50px] h-[50px] absolute top-[calc(50%-25px)] right-16 pointer-events-auto text-3xl bg-pink-600 text-white rounded-full"
       onClick={handleClick}
     >
       W
